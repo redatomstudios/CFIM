99041cb0c3828022de6e1ee7b31ec71b1d073b82
stRIker <albinin0002@gmail.com>
2012-12-17 20:27:00 +0530

    Initial Commit

---
+++ localhost.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 17, 2012 at 03:55 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cfim`
+--
+DROP DATABASE `cfim`;
+CREATE DATABASE `cfim` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `cfim`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attachments`
+--
+
+CREATE TABLE IF NOT EXISTS `attachments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `filename` varchar(50) NOT NULL,
+  `accessible By` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobtitle`
+--
+
+CREATE TABLE IF NOT EXISTS `jobtitle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `memberName` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `rank` int(5) NOT NULL,
+  `titleId` int(3) NOT NULL,
+  `status` varchar(2) NOT NULL,
+  `subordinates` varchar(100) NOT NULL,
+  `officeEmail` varchar(50) NOT NULL,
+  `otherEmail` varchar(50) NOT NULL,
+  `contactTel1` int(10) NOT NULL,
+  `contactTel2` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `leaderId` int(10) NOT NULL,
+  `sector` varchar(50) NOT NULL,
+  `subSector` varchar(50) NOT NULL,
+  `geoRegion` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `discussionDate` date NOT NULL,
+  `status` int(2) NOT NULL,
+  `members` varchar(100) NOT NULL,
+  `documents` varchar(100) NOT NULL,
+  `dealSize` int(11) NOT NULL,
+  `companyName` varchar(50) NOT NULL,
+  `companyAddress` text NOT NULL,
+  `contactPerson` varchar(50) NOT NULL,
+  `contactEmail` varchar(100) NOT NULL,
+  `contactTel` int(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
