28ba90f75fb0fd6dfd471ea570580019eeadf1a0
Max Akana <quiksilvrgreen@gmail.com>
2012-12-22 13:04:30 +0530

    Master JS file updated

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -8,17 +8,7 @@
 	<script src="<?= base_url() ?>resources/js/jquery-1.8.2.min.js"></script>
 	<script src="<?= base_url() ?>resources/js/jquery-ui-1.9.2.custom.min.js"></script>
 	<script src="<?= base_url() ?>resources/js/jquery.dataTables.min.js"></script>
-	<script>
-		jQuery(document).ready(function() {
-			$('table.data').dataTable({
-				"sScrollY": "350px",
-		        "bPaginate": false,
-		        "bScrollCollapse": true
-			});
-
-			$('.datePicker').datepicker({minDate: 0, numberOfMonths: 3});
-		});
-	</script>
+	<script src="<?= base_url() ?>resources/js/h2o.js"></script>
 </head>
 <body>
 	<header>
