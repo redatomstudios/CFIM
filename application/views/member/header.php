d6c414d2d0b936642c723f18e8834c39b39c96ab
Max Akana <quiksilvrgreen@gmail.com>
2012-12-31 02:28:58 +0530

    Change Password finished, notification needs to be added

--- application/views/member/header.php
+++ application/views/member/header.php
@@ -12,10 +12,12 @@
 	<title><?= $pageTitle[$currentPage] ?> :: Project Management System</title>
 	<link rel="stylesheet" href="<?= base_url() ?>resources/css/ui-lightness/jquery-ui-1.9.2.custom.min.css?<?= hash_file('crc32', 'resources/css/ui-lightness/jquery-ui-1.9.2.custom.min.css') ?>" />
 	<link rel="stylesheet" href="<?= base_url() ?>resources/css/jquery.dataTables.css?<?= hash_file('crc32', 'resources/css/jquery.dataTables.css') ?>" />
+	<link rel="stylesheet" href="<?= base_url() ?>resources/css/formalize.css?<?= hash_file('crc32', 'resources/css/formalize.css') ?>" />
 	<link rel="stylesheet" href="<?= base_url() ?>resources/css/member.css?<?= hash_file('crc32', 'resources/css/member.css') ?>" />
 	<script src="<?= base_url() ?>resources/js/jquery-1.8.2.min.js?<?= hash_file('crc32', 'resources/js/jquery-1.8.2.min.js') ?>"></script>
 	<script src="<?= base_url() ?>resources/js/jquery-ui-1.9.2.custom.min.js?<?= hash_file('crc32', 'resources/js/jquery-ui-1.9.2.custom.min.js') ?>"></script>
 	<script src="<?= base_url() ?>resources/js/jquery.dataTables.min.js?<?= hash_file('crc32', 'resources/js/jquery.dataTables.min.js') ?>"></script>
+	<script src="<?= base_url() ?>resources/js/jquery.formalize.min.js?<?= hash_file('crc32', 'resources/js/formalize.js') ?>"></script>
 	<script src="<?= base_url() ?>resources/js/h2o.js?<?= hash_file('crc32', 'resources/js/h2o.js') ?>"></script>
 	<script>
 		function getTime() {
