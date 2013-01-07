3379652f9cb1914d60352d9efca1123e715f43ca
striker <albinin0002@gmail.com>
2013-01-07 17:11:04 +0530

    Admin statistics completed

--- application/views/admin/statistics.php
+++ application/views/admin/statistics.php
@@ -1,22 +1,22 @@
 <script>
 	var subSector = [''];
 </script>
-<?= form_open_multipart('admin/') ?>
+<?= form_open_multipart('/admin/statistics') ?>
 <div class="gridTwo spaceTop">
 	<?= form_label('Project Leader', 'leader') ?>
-	<?= form_dropdown('leader', $projectLeader) ?>
+	<?= form_dropdown('leader', $leaders) ?>
 </div>
 <div class="gridTwo spaceTop">
 	<?= form_label('From', 'fromDate') ?>
-	<?= form_input(array('name' => 'fromDate', 'class' => 'datePicker') ?>
+	<?= form_input(array('name' => 'fromDate', 'class' => 'datePicker')) ?>
 </div>
 <div class="gridTwo spaceTop">
 	<?= form_label('Project Member', 'member') ?>
-	<?= form_dropdown('member', $projectMember) ?>
+	<?= form_dropdown('member', $projectMembers) ?>
 </div>
 <div class="gridTwo spaceTop">
 	<?= form_label('To', 'toDate') ?>
-	<?= form_input(array('name' => 'toDate', 'class' => 'datePicker') ?>
+	<?= form_input(array('name' => 'toDate', 'class' => 'datePicker')) ?>
 </div>
 <div class="gridOne spaceTop">
 	<?= form_label('Sector', 'sector') ?>
@@ -27,8 +27,8 @@
 	<?= form_dropdown('subsector', $subsectors) ?>
 </div>
 <div class="gridOne spaceTop">
-	<?= form_label('Region', 'geoRegion') ?>
-	<?= form_dropdown('geoRegion', $provinces) ?>
+	<?= form_label('Region', 'province') ?>
+	<?= form_dropdown('province', $provinces) ?>
 </div>
 <div class="gridOne spaceTop">
 	<?= form_label('Status', 'status') ?>
